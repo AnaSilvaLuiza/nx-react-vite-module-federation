2d728b35c10501b5d4b2c08f1b191c586dc62bcf
Ana Luiza Silva <ana.silva@vortal.biz>
2024-09-26 16:39:06 +0100

    added NODE_ENV

--- remote/src/app/app.tsx
+++ remote/src/app/app.tsx
@@ -1,11 +1,21 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+// import { useState } from 'react';
 import styles from './app.module.css';
+import { Button } from '@olmero/common-ui';
 
 export function App() {
+  // const [openInviteModal, setOpenInviteModal] = useState<boolean>(false);
+
   return (
     <>
-      <h1>Remote ALS Test</h1>
-      <p>Hello from Remote!</p>
+      <h1>Remote ALS Test 18</h1>
+      <Button.Root> Invite</Button.Root>   
+
+      {/* <InviteModal
+        handleClose={() => setOpenInviteModal(false)}
+        handleOpen={() => setOpenInviteModal(true)}
+        open={openInviteModal}
+      /> */}
     </>
   );
 }
