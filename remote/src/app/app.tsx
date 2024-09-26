072823b79a360ec544b821343e1abbcc7c68ec25
Ana Luiza Silva <ana.silva@vortal.biz>
2024-09-26 17:15:14 +0100

    added drawer component

--- remote/src/app/app.tsx
+++ remote/src/app/app.tsx
@@ -1,21 +1,53 @@
 
-// import { useState } from 'react';
+import { useState } from 'react';
+import { Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import styles from './app.module.css';
-import { Button } from '@olmero/common-ui';
+import { Button, Divider } from '@olmero/common-ui';
+import { Inbox, Mail } from '@mui/icons-material';
 
 export function App() {
-  // const [openInviteModal, setOpenInviteModal] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
 
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <Inbox /> : <Mail />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <Inbox/> : <Mail/>}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+  
   return (
     <>
-      <h1>Remote ALS Test 18</h1>
-      <Button.Root> Invite</Button.Root>   
-
-      {/* <InviteModal
-        handleClose={() => setOpenInviteModal(false)}
-        handleOpen={() => setOpenInviteModal(true)}
-        open={openInviteModal}
-      /> */}
+      <Button.Root  onClick={toggleDrawer(true)}> Invite</Button.Root>   
+      <Drawer open={open} onClose={toggleDrawer(false)}  anchor="right">
+        {DrawerList}
+      </Drawer>    
     </>
   );
 }
